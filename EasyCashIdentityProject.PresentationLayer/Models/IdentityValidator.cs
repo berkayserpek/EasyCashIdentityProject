2e59653ba3454d5d4b8b3abcb0d98fb44bfda1c5
Berkay Serpek <berkayserpek@gmail.com>
2023-05-14 02:22:57 +0300

    add register identity error translate

---
+++ EasyCashIdentityProject.PresentationLayer/Models/IdentityValidator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+	public class IdentityValidator : IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordTooShort",
+				Description = $"Parola en az {length} karakter olmalıdır."
+			};
+		}
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresUpper",
+				Description = "Lütfen en az 1 tane büyük harf giriniz."
+			};
+		}
+		public override IdentityError PasswordRequiresLower()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresLower",
+				Description = "Lütfen en az 1 tane rakam giriniz."
+			};
+		}
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresNonAlphanumeric",
+				Description = "Lütfen en az 1 tane özel karakter giriniz."
+			};
+		}
+	}
+}
